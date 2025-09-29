350eba3578f9b06e81ad60f30999fc30f6d5f5cb
Henry <git@thasler.org>
2025-09-29 18:36:45 +0200

    set up c++ skeleton and cmake

---
+++ tiny-wasm/tiny-wasm.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "modules/tiny-loader.hpp"
+
+int main(int argc, char const *argv[])
+{
+  if (argc < 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <wasm_file>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Loading WebAssembly file: " << argv[1] << std::endl;
+  tiny::Loader loader(argv[1]);
+  std::vector<uint8_t> bytecode = loader.getBytecode();
+
+  std::cout << "Loaded bytecode size: " << bytecode.size() << " bytes" << std::endl;
+  std::cout << "Bytecode (hex): ";
+  for (uint8_t byte : bytecode)
+  {
+    std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte) << " ";
+  }
+  std::cout << std::dec << std::endl;
+
+  return EXIT_SUCCESS;
+}
