71746368eb2c1f8d0c515f2e6fc28ffd7f949bed
Henry Thasler <Henry.Thasler@bmw.de>
2026-02-04 13:37:55 +0100

    add wrapper to generate multiple files

--- test-generator/generate.py
+++ test-generator/generate.py
@@ -1,3 +1,6 @@
+import re
+import sys
+from pathlib import Path
 import json
 from jinja2 import Environment, FileSystemLoader
 
@@ -13,4 +16,22 @@ def generate_from_json(json_input: str, output_file: str = "generated.cpp"):
         f.write(rendered)
 
 if __name__ == '__main__':
-    generate_from_json("./tests/assets/linear-memory-data.json", "./tests/linear_memory_data_test.cpp")
+    if len(sys.argv) < 2:
+        print("Usage: python generate.py path_to_jsons [output_directory]")
+        sys.exit(1)
+    
+    allow_list = ["linear-memory-data.json"]
+
+    # find all json files
+    for input_file in Path(sys.argv[1]).rglob('*.json'):
+        # for now, we only allow a specific set of files
+        if input_file.name not in allow_list:
+            continue
+
+        # extract filename without extension
+        output_file = Path(sys.argv[2]) / (input_file.stem + '_test.cpp') if len(sys.argv) > 2 else None
+        print("generating test from '{}' to '{}'".format(input_file, output_file))
+        if output_file:
+            generate_from_json(input_file, output_file)
+    
+    print("\033[92mDone!\033[0m")
